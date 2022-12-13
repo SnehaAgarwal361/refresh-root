8607956bdc193cdba68a06b090d4330ae2616a69
akhan347 <abdul.khan@aexp.com>
2022-12-13 11:40:45 +0530

    fixed description of applications

--- src/applicationList.js
+++ src/applicationList.js
@@ -1,7 +1,7 @@
 export const applicationList = [
   {
     friendlyName: 'Customer Inbound',
-    description: 'Customer Inbound interact with Inbound channels to provide information on  customer\'s KYC Refresh Reviews',
+    description: 'Customer Inbound interacts with Inbound channels to provide information on customer\'s KYC Refreshes',
     isEngineImplementation: false,
     id: 'CIN',
   },
@@ -13,25 +13,25 @@ export const applicationList = [
   },
   {
     friendlyName: 'Manager',
-    description: 'Manager provides an api to read and change the application properties',
+    description: 'Manager provides an api to read and change the application properties, scheduled jobs and triggers',
     isEngineImplementation: false,
     id: 'MGR',
   },
   {
     friendlyName: 'Refresh Workflow Individual',
-    description: '.....',
+    description: 'Refresh Workflow Individual decides how and where a refresh should take place',
     isEngineImplementation: false,
     id: 'RWI',
   },
   {
     friendlyName: 'Trigger',
-    description: '.....',
+    description: 'Trigger decides when a refresh should take place and it initiates a refresh for an eligible Customer.',
     isEngineImplementation: false,
     id: 'TRG',
   },
   {
     friendlyName: 'Tracker',
-    description: 'Tracker keeps track of data blocks need to be refreshed at customer level',
+    description: 'Tracker keeps track of the data blocks that need to be refreshed at a customer level',
     isEngineImplementation: false,
     id: 'TRK',
   },
