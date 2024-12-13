a91761d7cfd75af9f207b59edd014b5f4fe0ca1c
kbolourc <Kit.Bolourchi@aexp.com>
2024-12-13 11:25:25 +0000

    WIP

--- test-setup.js
+++ test-setup.js
@@ -1,6 +1,6 @@
 // require('amex-jest-preset-react/jest-setup');
 require('@babel/polyfill');
-require('@testing-library/jest-dom/extend-expect');
+// require('@testing-library/jest-dom/extend-expect');
 
 // prevents us from needing to mock out content-security-policy lib which requires this to be set
 process.env.ONE_CLIENT_CSP_REPORTING_URL = 'https://example.com/_/report/security/csp-violation';
